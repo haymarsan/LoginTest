c922ceb4bfb908e7bc05d5cc2085b778aec7b138
Hay Mar San <haymarsan11@gmail.com>
2019-11-18 14:11:28 +0630

    First commit

---
+++ app/src/main/java/com/haymarsan/loginactivity/ui/login/LoggedInUserView.kt
@@ -0,0 +1,9 @@
+package com.haymarsan.loginactivity.ui.login
+
+/**
+ * User details post authentication that is exposed to the UI
+ */
+data class LoggedInUserView(
+    val displayName: String
+    //... other data fields that may be accessible to the UI
+)
